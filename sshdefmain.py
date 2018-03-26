10b54e8b5d58a05197a482c4f3a0d04a5713b8a4
shansclensky <shansclensky66@gmail.com>
2018-03-26 01:15:45 -0400

    ssh done

---
+++ sshdefmain.py
@@ -0,0 +1,30 @@
+import sys
+import paramiko
+#import pdb
+#function definition
+#def conn(host,port,username,password):
+#host = sys.argv[1]
+#username=sys.argv[2]
+#password =sys.argv[3]
+#pdb.set_trace()
+def conn(hostname,port,username,password):
+    rm = paramiko.SSHClient()
+    rm.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    rm.connect(hostname=hostname,port=port , username=username, password=password)
+    stdin, stdout, stderr = rm.exec_command("ls")
+    val =stdout.read()
+    print val
+    return val
+
+if __name__ == "__main__":
+    hostname=sys.argv[1]
+    port=int(sys.argv[2])
+    username=sys.argv[3]
+    password =sys.argv[4]
+    conn(hostname,port,username,password)
+    
+
+ 
+
+
+
