313b3de0168bfd9575f7341fae6ecbd700e41e0b
shansclensky <shansclensky66@gmail.com>
2018-03-26 03:19:31 -0400

    sshdone properly

--- sshdefmain.py
+++ sshdefmain.py
@@ -8,21 +8,22 @@ import paramiko
 #password =sys.argv[3]
 #pdb.set_trace()
 def conn(hostname,port,username,password):
+    """ connection to server is established and the output is read from server."""
     rm = paramiko.SSHClient()
     rm.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     rm.connect(hostname=hostname,port=port , username=username, password=password)
     stdin, stdout, stderr = rm.exec_command("ls")
     val =stdout.read()
-    print val
+    #print val 
     return val
-
 if __name__ == "__main__":
     hostname=sys.argv[1]
     port=int(sys.argv[2])
     username=sys.argv[3]
     password =sys.argv[4]
-    conn(hostname,port,username,password)
-    
+    a=conn(hostname,port,username,password)
+    print a
+    print conn.__doc__
 
  
 
