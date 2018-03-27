2f5d75373c23588d3075d9f66a56d2aae3119e57
shansclensky <shansclensky66@gmail.com>
2018-03-27 07:20:38 -0400

    sshdef1 done

--- sshdefmain.py
+++ sshdefmain.py
@@ -1,5 +1,6 @@
 import sys
 import paramiko
+import os
 #import pdb
 #function definition
 #def conn(host,port,username,password):
@@ -7,28 +8,29 @@ import paramiko
 #username=sys.argv[2]
 #password =sys.argv[3]
 #pdb.set_trace()
-def conn(hostname,port,username,password,key_filename):
+class sshclient:
+    def __init__(self,hostname,port,password,key_filename):
+    #def conn(hostname,port,username,password,key_filename):
     """ connection to server is established and the output is read from server."""
     #username password do ssh or keypassed means do ssh accodingly  as specified in the 
-    clientssh = paramiko.SSHClient()
-    clientssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    res = clientssh.connect(hostname=hostname,port=port , username=username, password=password,key_filename=key_filename)
+    self. paramiko.SSHClient()
+    self.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    self.connect(hostname=hostname,port=port , username=username, password=password,key_filename=key_filename)
     #stdin, stdout, stderr = rm.exec_command("")
     #stdout.read()
-    return res
-
-def get_cpuutilization(res):
-    stdin,stdout,stderr = clientssh.exec_command("top")
+    
+def get_cpuutilization(self):
+    stdin,stdout,stderr =self.exec_command("top")
     cpu_info=stdout.read()
     return cpu_info
 
-def get_memoryutilization(res):
-    stdin,stdout,stderr = clientssh.exec_commad("free -m")
+def get_memoryutilization(self):
+    stdin,stdout,stderr = self.exec_commad("free -m")
     memory_info=stdout.read()
     return memory_info
 
-def get_portstatistics(res):
-    stdin.stdout,stderr = clientssh.exec_command("netstat -i")
+def get_portstatistics(self):
+    stdin.stdout,stderr = self.exec_command("netstat -i")
     port_info=stdout.read()
     return port_info
 if __name__ == "__main__":
@@ -37,20 +39,19 @@ if __name__ == "__main__":
     username=sys.argv[3]
     password =sys.argv[4]
     key_filename="/opt/testkey.pem"
+    s1 = sshclient(hostname,port,username,password,key_filename)
     if argv[4]==0:
-       a=conn(hostname,port,username,password)
-       a0=get_cpuutilizaton(res)
-       a1=get_memoryutilization(res)
-       a2=get_portstatistics(res)
+       a0= s1.get_cpuutilizaton()
+       a1= s1.get_memoryutilization()
+       a2=s1.get_portstatistics(res)
        print a0,a1,a2
        #print conn.__doc__
+    
     else:
-        try:
-           
-        b =comm(hostnamem,port,username,keyfile_name)
-        b0= get_cpuutilizaton(res)
-        b1=get_memoryutilization(res)
-        b2=get_portstatistics(res)
+       b =comm(hostname,port,username,keyfile_name)
+       b0=s1.get_cpuutilizaton()
+       b1=s2.get_memoryutilization()
+       b2=s3.get_portstatistics()
        print b0,b1,b2 
  
 
