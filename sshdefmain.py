29e206641cf943d9485a34f421d44b4ab6c8716b
shansclensky <shansclensky66@gmail.com>
2018-03-27 11:03:09 -0400

    sshdef3 done

--- sshdefmain.py
+++ sshdefmain.py
@@ -46,19 +46,19 @@ if __name__ == "__main__":
     #port=int(sys.argv[2])
     username=sys.argv[2]
     password =sys.argv[3]
-    key_filename=sys.argv[4]
+    #key_filename=sys.argv[4]
     parser = argparse.ArgumentParser(description="validation for logging")
     parser.add_argument('--hostname',help='enter valid hostname',required=True)
     parser.add_argument('--username',help='enter valid username',required=True)
     parser.add_argument('--password',help='enter the password',required=False)
-    parser.add_argument('--key_filename',help='specify the key_filename path',required=False)
+    #parser.add_argument('--key_filename',help='specify the key_filename path',required=False)
     args = vars(parser.parse_args())
     if not(args['--username'] or args['--hostname']):
          print "prime argument missing"
     elif  not args['--password']): 
          print"password missing "
-    elif not args['--key_filename']):
-         print"key_filename missing"
+    #elif not args['--key_filename']):
+     #    print"key_filename missing"
     else:
          s1 = Sshclient(hostname,port,username,password)
          a0= s1.get_cpuutilizaton()
