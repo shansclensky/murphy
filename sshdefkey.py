1c51840a17a319883727ee56476b760c1e9b5118
shansclensky <shansclensky66@gmail.com>
2018-03-27 02:32:54 -0400

    sshkey done

--- sshdefkey.py
+++ sshdefkey.py
@@ -14,7 +14,7 @@ def conn(hostname,username,keyfile):
     """ connection to server is established and the output is read from server."""
     rm = paramiko.SSHClient()
     rm.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    rm.connect(hostname=hostname,username=username,key=key)
+    rm.connect(hostname=hostname,username=username,key_filename=key_filename)
     stdin, stdout, stderr = rm.exec_command("ls")
     val =stdout.read()
     #print val
@@ -22,8 +22,8 @@ def conn(hostname,username,keyfile):
 if __name__ == "__main__":
     hostname=sys.argv[1]
     username=sys.argv[2]
-    key =paramiko.RSAKey.from_private_key_file("/opt/testkey.pem")
-    a=conn(hostname,username,key)
+    key_filename ="/opt/testkey.pem"
+    a=conn(hostname,username,key_filename)
     print a
     print conn.__doc__
 
