3e6029f81cea7b5b174102c9dcea03c74081b546
shansclensky <shansclensky66@gmail.com>
2018-03-27 07:44:17 -0400

    sshdef1 done

--- sshdefmain.py
+++ sshdefmain.py
@@ -9,13 +9,13 @@ import os
 #password =sys.argv[3]
 #pdb.set_trace()
 class sshclient:
-    def __init__(self,hostname,port,password,key_filename):
+    def __init__(self,hostname=10.16.86.156,port=22,password,key_filename):
     #def conn(hostname,port,username,password,key_filename):
     """ connection to server is established and the output is read from server."""
     #username password do ssh or keypassed means do ssh accodingly  as specified in the 
-    self. paramiko.SSHClient()
-    self.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    self.connect(hostname=hostname,port=port , username=username, password=password,key_filename=key_filename)
+    self.ssh=paramiko.SSHClient()
+    self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    self.ssh.connect(hostname=hostname,port=port , username=username, password=password,key_filename=key_filename)
     #stdin, stdout, stderr = rm.exec_command("")
     #stdout.read()
     
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     if argv[4]==0:
        a0= s1.get_cpuutilizaton()
        a1= s1.get_memoryutilization()
-       a2=s1.get_portstatistics(res)
+       a2=s1.get_portstatistics()
        print a0,a1,a2
        #print conn.__doc__
     
